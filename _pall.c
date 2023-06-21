064f0b58ca0596cbea11de68fc027aad83cc45ff
johnechono <johninalegwu22@gmail.com>
2023-06-21 06:50:00 -0700

    pall

--- _pall.c
+++ _pall.c
@@ -1,12 +1,13 @@
 #include "monty.h"
 
 /**
- * pall - will print the stack
- * @stack: the given stack
- * @line_count: the number of lines
- * Return: void or null
+ * pall - prints the stack
+ * @stack: stack given by main
+ * @line_cnt: ammount of lines
+ *
+ * Return: void
  */
-void pall(stack_t **stack, unsigned int line_count __attribute__((unused)))
+void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
 {
 	print_stack(*stack);
 }
